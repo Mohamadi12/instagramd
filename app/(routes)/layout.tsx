d8b9d71b5912cece1ddf65749a9f1878f764e73d
Nana Mohamadi <adilnana78@gmail.com>
2024-10-30 00:08:40 +0100

    Mobile navigation

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -3,6 +3,14 @@ import localFont from "next/font/local";
 import "@radix-ui/themes/styles.css";
 import ".././globals.css";
 import { Theme } from "@radix-ui/themes";
+import Link from "next/link";
+import {
+  CameraIcon,
+  HomeIcon,
+  LayoutGridIcon,
+  SearchIcon,
+  UserIcon,
+} from "lucide-react";
 
 const geistSans = localFont({
   src: ".././fonts/GeistVF.woff",
@@ -30,7 +38,31 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased p-4`}
       >
-        <Theme>{children}</Theme>
+        <Theme>
+          {children}
+          <div className="fixed bottom-0 px-4 py-2 left-0 right-0">
+            <div className="max-w-sm mx-auto flex justify-between text-gray-600 *:size-12 *:flex *:items-center *:justify-center">
+              <Link href="/">
+                <HomeIcon />
+              </Link>
+              <Link href="/search">
+                <SearchIcon />
+              </Link>
+              <Link
+                href="/create"
+                className="bg-gradient-to-tr from-ig-orange to-ig-red size-12 flex items-center justify-center rounded-full text-white relative -top-12"
+              >
+                <CameraIcon />
+              </Link>
+              <Link href="/browse">
+                <LayoutGridIcon />
+              </Link>
+              <Link href="/profile" className="text-ig-red">
+                <UserIcon />
+              </Link>
+            </div>
+          </div>
+        </Theme>
       </body>
     </html>
   );
