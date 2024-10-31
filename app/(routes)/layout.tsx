73c32ee068f80bd7632a9f63d14b494b85bb6841
Nana Mohamadi <adilnana78@gmail.com>
2024-10-31 17:35:47 +0100

    Mobile navigation extras

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -3,14 +3,7 @@ import localFont from "next/font/local";
 import "@radix-ui/themes/styles.css";
 import ".././globals.css";
 import { Theme } from "@radix-ui/themes";
-import Link from "next/link";
-import {
-  CameraIcon,
-  HomeIcon,
-  LayoutGridIcon,
-  SearchIcon,
-  UserIcon,
-} from "lucide-react";
+import MobileNav from "../components/MobileNav";
 
 const geistSans = localFont({
   src: ".././fonts/GeistVF.woff",
@@ -40,28 +33,7 @@ export default function RootLayout({
       >
         <Theme>
           {children}
-          <div className="fixed bottom-0 px-4 py-2 left-0 right-0">
-            <div className="max-w-sm mx-auto flex justify-between text-gray-600 *:size-12 *:flex *:items-center *:justify-center">
-              <Link href="/">
-                <HomeIcon />
-              </Link>
-              <Link href="/search">
-                <SearchIcon />
-              </Link>
-              <Link
-                href="/create"
-                className="bg-gradient-to-tr from-ig-orange to-ig-red size-12 flex items-center justify-center rounded-full text-white relative -top-12"
-              >
-                <CameraIcon />
-              </Link>
-              <Link href="/browse">
-                <LayoutGridIcon />
-              </Link>
-              <Link href="/profile" className="text-ig-red">
-                <UserIcon />
-              </Link>
-            </div>
-          </div>
+          <MobileNav/>
         </Theme>
       </body>
     </html>
